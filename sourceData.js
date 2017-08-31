19f36528a379138d03a71f20df863c2407eab8ff
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-30 18:58:00 -0700

    added updated constructor function, and 3 add to html methods

---
+++ sourceData.js
@@ -0,0 +1,27 @@
+var sourceData = [
+    
+{
+    title:          'About Me', 
+    URL:            'www.github.com', 
+    description:    'All about Christina Mills',
+    img:            'file.pjg',
+
+},
+
+{
+    title:          'Salmon Cookies', 
+    URL:            'www.github.com', 
+    description:    'All about COOKIES',
+    img:            'file.pjg',
+
+},
+
+{
+    title:          'Bus Mall', 
+    URL:            'www.github.com', 
+    description:    'Elevating Bus purchasing for all',
+    img:            'file.pjg',
+
+}
+
+]
