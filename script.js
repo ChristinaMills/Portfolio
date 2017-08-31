6d54b199475f3777752c2642d1452aa1ed98111d
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-08-31 16:39:28 -0700

    finished MVP

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ projects.forEach(function(project) {
 function handleNav () {
   $('.tab').click( function(){
     $('.tab-content').hide();  
-    // var clickedTab = $(this);
-    // $()
+    var clickedTab = $(this).attr('data-content');
+    $('#' + clickedTab).show();
 
   })  
 }
