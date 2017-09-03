4d7507137969aa07c6fdc6f42436580df17d1d16
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-09-03 16:11:51 -0700

    added JSON file, added loadAll function and conditional local storage function, broke my code

--- script.js
+++ script.js
@@ -9,24 +9,36 @@ function Project (sourceData) {
 }
 
 Project.prototype.toHtml = function() {
-    var $newProject = $('#proj-temp').clone();
-    $newProject.removeClass('template');
+    var template = $('#project-template').html()
+    console.log(template)
+    var templateFiller = Handlebars.compile(template)
 
-    $newProject.find('#proj-title').text(this.title);
-    $newProject.find('#URL').html(this.URL);
-    $newProject.find('#description').text(this.description);
-    $newProject.find('#img').attr('src', this.img)
-    //$newProject.find('#img').html(this.img);
+    var filledTemplate = templateFiller(this)
+    console.log("this is the :", filledTemplate)
+    console.log("this is this", this)
+ 
+  
+    return filledTemplate;
 
-    console.log($newProject)
-    return $newProject;
+    // var $newProject = $('#proj-temp').clone();
+    // $newProject.removeClass('template');
+
+    // $newProject.find('#proj-title').text(this.title);
+    // $newProject.find('#URL').html(this.URL);
+    // $newProject.find('#description').text(this.description);
+    // $newProject.find('#img').attr('src', this.img)
+    // //$newProject.find('#img').html(this.img);
+
+    // console.log($newProject)
+    // return $newProject;
 }
 
+Project.loadAll = function(sourceData) {
 
-sourceData.forEach(function(projectObject) {
+    sourceData.forEach(function(projectObject) {
     projects.push(new Project(projectObject));
-  });
-
+    });
+};
 
 //I don't know why this doesn't work!
 // var projectList = document.getElementById('project-list');
@@ -48,3 +60,19 @@ function handleNav () {
 handleNav();
 // $('#project-section').hide();
 // $('#about-section').hide();
+
+Project.fetchAll = function() {
+    if(localStorage.sourceData) {
+        Project.loadAll(JSON.parse(localStorage.getItem('sourceData')));
+    }
+    else {
+        $.getJSON('sourceData.json')
+        .done(data => {
+          localStorage.setItem('sourceData', JSON.stringify(data));
+          Project.loadAll(data)
+        })
+        .fail(() => {
+          alert('nope');
+        });
+    }
+}
